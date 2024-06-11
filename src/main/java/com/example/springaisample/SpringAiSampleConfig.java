6c0e94d238496dc4c20b204a7a108843fd64272c
takuro-furukawa <tic.tak.guchuan@gmail.com>
2024-06-11 14:30:50 +0900

    change vector store to neo4j

--- src/main/java/com/example/springaisample/SpringAiSampleConfig.java
+++ src/main/java/com/example/springaisample/SpringAiSampleConfig.java
@@ -1,30 +1,15 @@
 package com.example.springaisample;
 
 import org.springframework.ai.ollama.api.OllamaApi;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.autoconfigure.r2dbc.R2dbcProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.client.RestClient;
 
 @Configuration
 public class SpringAiSampleConfig {
 
     @Bean
     public OllamaApi getOllamaApi() {
-        RestClient.Builder builder = RestClient.builder();
-        return new OllamaApi("http://localhost:11434/", builder);
+        return new OllamaApi();
     }
 
-    @Bean
-    public JdbcTemplate getJdbcTemplate(DataSourceProperties databaseProperties) {
-        return new JdbcTemplate(
-                DataSourceBuilder.create()
-                        .url(databaseProperties.getUrl())
-                        .username(databaseProperties.getUsername())
-                        .password(databaseProperties.getPassword())
-                        .build());
-    }
 }
